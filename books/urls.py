cb46c031fb5594dafa4a17a08e5cbcad20da6557
Nima Patel <nima2001patel@gmail.com>
2024-12-18 14:09:42 +0530

    docker_changes_add

--- books/urls.py
+++ books/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from .views import register_user, login_user, logout_user, books_page, BookListView, BookDetailView, book_list_page
+from .views import register_user, login_user, logout_user, books_page, BookListView, BookDetailView, book_list_page , BookRawQueryView
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -23,7 +23,7 @@ urlpatterns = [
     path('data/', book_list_page, name='data-page'),
     path('api/books/', BookListView.as_view(), name='book-list'),
     path('api/books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    
+    path('books/raw/', BookRawQueryView.as_view(), name='book-raw-query'), 
     # Swagger URLs
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
